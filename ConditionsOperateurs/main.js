7325940e9634c8d3851325bc94683dd526d16e58
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2021-05-16 08:20:07 +0200

    Create main.js

---
+++ ConditionsOperateurs/main.js
@@ -0,0 +1,24 @@
+// |||||| LEARNING - OPERATORS |||||| //
+// == // (égal à)
+var v = 3;
+var w = "3";
+if (v==w) {
+  console.log("v==w");
+} else {
+  console.log("!v==w");
+}
+// === // (Strictement égal à)
+var v = 3;
+var w = "3";
+if (v===w) {
+  console.log("v===w");
+} else {
+  console.log("!v===w");
+}
+var v = 3;
+var w = 3;
+if (v===w) {
+  console.log("v===w");
+} else {
+  console.log("!v===w");
+}
