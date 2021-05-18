b075201bd48dc2a124a29f60e97c00197440cd94
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2021-05-18 10:51:22 +0200

    Create main.js

---
+++ Boucles/main.js
@@ -0,0 +1,21 @@
+// |||||| LEARNING - BOUCLES |||||| //
+// while //
+if (true) {
+  let a = 0;
+  let arr = [];
+  while (a <= 100) {
+    console.log(a);
+    if (a > 0) { arr.push(a); }
+    a+=3;
+    console.log(arr);
+  }
+}
+// do while //
+if (true) {
+  
+}
+// for //
+
+// for in //
+
+// for of //
