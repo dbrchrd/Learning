1692a43beb5842de4ef36b7e79b24d36c41ce6c3
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2021-05-19 14:09:43 +0200

    Update main.js

--- Fonctions/main.js
+++ Fonctions/main.js
@@ -1,25 +1,28 @@
 // |||||| LEARNING - FONCTIONS |||||| //
 // function "normale" //
 if (true) {
+  let c = "result : ";
   function functionName(a, b) {
     // Instructions
-    return a * b;
+    return c + a * b;
   }
   console.log(functionName(31, 66));
 }
 // function "avec variable" //
 if (true) {
+  let c = "result : ";
   let functionName = function(a, b) {
     // Instructions
-    return a * b;
+    return c + a * b;
   }
   console.log(functionName(31, 66));
 }
 // function "flèchée" //
 if (true) {
+  let c = "result : ";
   let functionName = (a, b) => {
     // Instructions
-    return a * b;
+    return c + a * b;
   };
   console.log(functionName(31, 66));
 }
