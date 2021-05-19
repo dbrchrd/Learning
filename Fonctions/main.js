3db2ec83b9d181384d021925f971a910f656554b
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2021-05-19 13:41:27 +0200

    Create main.js

---
+++ Fonctions/main.js
@@ -0,0 +1,9 @@
+// |||||| LEARNING - FONCTIONS |||||| //
+// function "normale" //
+if (true) {
+  function functionName(a, b) {
+    // Instructions
+    return a * b;
+  }
+  console.log(functionName(31, 66));
+}
